a18caebc52658b46b576eb7ea7ad854322ac5080
Christopher Schmitt <schmitt@heatvision.com>
2017-06-22 16:19:47 -0500

    Small editorial update for consistency

--- vidtime.sh
+++ vidtime.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Description: Loops through all .mp4 files to find total length of time
+# Description: Loops through all .mp4 files in a folder to find total length of time
 
 function show_time () {
     num=$1
